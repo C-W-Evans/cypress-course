cb2f8bffaa6e443f987080a534614833293192d5
C-W-Evans <164054958+C-W-Evans@users.noreply.github.com>
2025-02-27 21:27:12 +0100

    correct cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    excludeSpecPattern: 'e2e/examples/*.cy.js*',
+    excludeSpecPattern: '**e2e/examples/*.cy.js*',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
